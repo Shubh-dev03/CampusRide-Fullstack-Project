7ed78f9f3b32eb8443f86e05bbdd597c4a744a2a
Shubham <cidkagenou108@gmail.com>
2026-04-19 18:44:47 +0530

    api correction

--- CampusRide-Backend/server.js
+++ CampusRide-Backend/server.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
 
 //Routes
 app.use("/api/auth", authRoutes);
-app.use("/api/user", userRoutes);
+app.use("/api/users", userRoutes);
 app.use("/api/rides", rideRoutes);
 app.use(errorMiddleware);
 
